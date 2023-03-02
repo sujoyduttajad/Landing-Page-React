afb59724b1dbe94752b1a930f31a409b65634a09
Ali raza <72508498+Aliraza2686@users.noreply.github.com>
2023-03-01 22:55:32 -0800

    updated Data.js
    just added one more suitable slogan

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -7,7 +7,7 @@ export const homeObjOne = {
     topLine: 'Marketing Agency',
     headline: 'Lead Generation Specialist for Online Businesses',
     description:
-      'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+      'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals. Strategic thinking for sustainable growth',
     buttonLabel: 'Get Started',
     imgStart: '',
     img: require('../../images/svg-1.svg'),
